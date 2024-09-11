47bb2c869adbe74aca02f2354ed1cb9753f34a94
Justin Simon <justinsimon@Justins-MBP.lan>
2024-09-11 05:07:26 -0700

    Initial commit

---
+++ MVCwAPI/Services/ToDoAPIService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;   
+using System.Net.Http;
+//this is the namespace for the extension method for deserializing JSON
+using System.Net.Http.Json;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using MVCwAPI.Models;
+namespace MVCwAPI.Services
+{
+    public class ToDoAPIService
+    {
+        niggersss
+        private readonly httpclient _client;
+        
+    }
+}
