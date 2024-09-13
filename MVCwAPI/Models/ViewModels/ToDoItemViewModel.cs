a42cc6e13542bb2a6138e9b1f9a5519c031bf040
Justin Simon <justinsimon@Justins-MBP.lan>
2024-09-13 14:16:05 -0700

    Added ViewModels for passing data from controller to the view

---
+++ MVCwAPI/Models/ViewModels/ToDoItemViewModel.cs
@@ -0,0 +1,10 @@
+namespace MVCwAPI.Models.ViewModels
+{
+    public class ToDoItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        
+    }
+}
